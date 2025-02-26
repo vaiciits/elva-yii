aa687034ee8a94ebacb3f86f93c401cbe1cc2280
vaiciits <vaicekauskis.m@gmail.com>
2025-02-26 21:01:27 +0200

    feat: add seeding for employees and work items

--- console/services/SeedService.php
+++ console/services/SeedService.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace console\services;
 
 use common\models\ConstructionSite;
+use common\models\Employee;
+use common\models\WorkItem;
 use Exception;
 use Faker\Factory;
 use Faker\Generator;
+use yii\db\ActiveRecord;
 
 /**
  * Data seeder for db.
@@ -21,11 +24,19 @@ class SeedService
         $this->faker = Factory::create();
     }
 
+    /**
+     * @throws Exception
+     */
     public function seed(): void
     {
         $this->seedConstructionSites();
+        $this->seedEmployees();
+        $this->seedWorkItems();
     }
 
+    /**
+     * @throws Exception
+     */
     public function seedConstructionSites(): void
     {
         for ($i = 0; $i < 10; $i++) {
@@ -33,11 +44,66 @@ class SeedService
             $site->name = $this->faker->company;
             $site->location = $this->faker->address;
             $site->area = $this->faker->randomFloat(2);
-            $site->access = $this->faker->randomElement([1, 2, 3]);
+            $site->access = $this->getRandomAccessLevel();
 
-            if (!$site->save()) {
-                throw new Exception(var_export($site->getErrors(), true));
-            }
+            $this->save($site);
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function seedEmployees(): void
+    {
+        for ($i = 0; $i < 20; $i++) {
+            $employee = new Employee();
+            $employee->name = $this->faker->firstName;
+            $employee->surname = $this->faker->lastName;
+            $employee->birthdate = $this->faker->date('Y-m-d');
+            $employee->access = $this->getRandomAccessLevel();
+            $employee->role = $this->faker->randomElement([1, 2, 3]);
+
+            $this->save($employee);
+        }
+    }
+
+    public function seedWorkItems(): void
+    {
+        for ($i = 0; $i < 42; $i++) {
+            $item = new WorkItem();
+            $item->name = $this->faker->sentence(5, true);
+            $item->description = $this->faker->sentences(20, true);
+            $item->construction_site_id = ConstructionSite::find()
+                ->select('id')
+                ->orderBy('NEWID()')
+                ->limit(1)
+                ->scalar();
+            $item->employee_id = Employee::find()
+                ->select('id')
+                ->orderBy('NEWID()')
+                ->limit(1)
+                ->scalar();
+
+            $this->save($item);
+        }
+    }
+
+    private function getRandomAccessLevel(): int
+    {
+        return $this->faker->randomElement([1, 2, 3]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function save(ActiveRecord $record): void
+    {
+        if (!$record->save()) {
+            throw new Exception(sprintf(
+                'Failed to save %s model: %s',
+                $record::class,
+                var_export($record->getErrors(), true),
+            ));
         }
     }
 }
